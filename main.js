e962f022ed2669f2c4662ab9441b7c82fff0a6ad
Norbus <odornorbert.on@gmail.com>
2021-04-13 13:41:28 +0200

    7,8

---
+++ main.js
@@ -0,0 +1,51 @@
+window.addEventListener("load",init);
+
+function ID (nev) {
+    return document.getElementById(nev);
+}
+
+function $(nev){
+    return document.querySelectorAll(nev);
+}
+
+function init () {
+
+    for (var i=0; i <3; i++) {
+        $("#bal img")[i].addEventListener("click", megjelenit, KepKozep);
+    }
+}
+function megjelenit() {
+    this.style.display="none";
+    var kepsrc=this.src;
+    document.querySelector('#csonak').innerHTML+=" <img src="+kepsrc+" alt='kecske'>"; 
+    
+}
+
+/* function KepKozep () {
+
+    /*if (display=="none"){
+
+    document.querySelector('#article').insertAdjacentHTML(
+      `<div class="kepek">
+      <img src="kepek/kecske2.png" alt="kecske">
+      <img src="kepek/kaposzta.png" alt="kaposzta">
+      <img src="kepek/farkas.png" alt="farkas">
+      </div>`      
+    )
+    this.style.display="block";
+    this.style.margin-left == auto;
+    this.style.margin-right==auto;
+  }
+}*/
+
+var  kecske=kepek/kecske2.png;
+var kaposzta =kepek/kaposzta.png;
+var farkas= kepek/farkas.png;
+
+function Alertablak() {
+    if (kecske.style.display=="block" &&  kaposzta.style.display=="block") {
+        alert("Helytelen választás!");
+    }else if (farkas.style.display=="block" &&  kecske.style.display=="block"){
+        alert("Helytelen választás!");
+    }
+}   
